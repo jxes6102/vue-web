7413438e1212d9f775cdb1dd187d79b04263def6
unknown <jxes6102@gamil.com>
2022-09-07 11:37:52 +0800

    temp09071137

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+  },
+  // rules: {
+  //   'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  //   'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  // }
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'comma-dangle': [
+      'error',
+      'always-multiline',
+    ],
+    'radix': ['error', 'as-needed'],
+    'no-var': 'error',
+    'no-unused-vars': 'warn',
+    'prefer-promise-reject-errors': 'off',
+    'line-comment-position': [
+      'error', {
+        'position': 'above',
+      },
+    ],
+  },
+}
