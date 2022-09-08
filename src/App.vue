ef5caacb16d9a1885d73a2330a9b4534f10bd30a
jxes6102 <jxes6102@gamil.com>
2022-09-08 17:01:42 +0800

    temp09081700

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,4 @@
 <template lang='pug'>
-nav
-  router-link(to="/") Home
-  ||
-  router-link(to="/about") About
 router-view
 </template>
 <script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import Vue from 'vue'
 import App from './App.vue'
 // import './registerServiceWorker'
 import 'normalize.css/normalize.css'
@@ -7,5 +6,8 @@ import './tailwind/index.css'
 import router from './router'
 import store from './store'
 import i18n from './lang/plugins/i18n'
-
-createApp(App).use(store).use(router).use(i18n).mount('#app')
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+createApp(App).use(store).use(router)
+.use(ElementPlus)
+.use(i18n).mount('#app')
