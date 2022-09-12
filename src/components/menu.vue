9cb2681163344a075a015d5157934bdfd13991e3
jxes6102 <jxes6102@gamil.com>
2022-09-12 10:44:55 +0800

    temp09121045

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -3,7 +3,7 @@ div(class="w-auto h-[100vh] border-r-2")
   div(class="w-auto  p-2")
     el-button(:icon="Switch" class="w-full" @click="isCollapse = !isCollapse")
   el-menu(
-    class=""
+    :default-active='defaultActive'
     :collapse='isCollapse'
     background-color="#ffffff"
     @open='handleOpen'
@@ -11,7 +11,7 @@ div(class="w-auto h-[100vh] border-r-2")
     style="border-right: 0px;"
     )
     el-menu-item(
-      v-for="(item, index) of menuData" :key="index"
+      v-for="(item, index) of nowMenu" :key="index"
       :index='index + ""'
       @click="toLink(item)"
       )
@@ -46,13 +46,14 @@ div(class="w-auto h-[100vh] border-r-2")
       setup() {
         const route = useRoute()
         const router = useRouter()
-        const routeData = ref({
+        const defaultActive = ref(null)
+        const menuData = ref({
           original: [
             {name: '首頁', icon:'House',route:'/'},
-            {name: '遊戲', icon:'MagicStick',route:'/game'},
             {name: '報表', icon:'document',route:'/form'},
             {name: '圖片', icon:'Picture',route:'/picture'},
-            {name: '工具 / 樣式', icon:'Handbag',route:'/tool'}
+            {name: '工具 / 樣式', icon:'Handbag',route:'/tool'},
+            {name: '遊戲', icon:'MagicStick',route:'/game'},
           ],
           game: [
             {name: '首頁', icon:'House',route:'/'},
@@ -61,13 +62,7 @@ div(class="w-auto h-[100vh] border-r-2")
             {name: '踩地雷', icon:'MagicStick'},
           ],
         })
-        const menuData = ref([
-          {name: '首頁', icon:'House',route:'/'},
-          {name: '遊戲', icon:'MagicStick',route:'/game'},
-          {name: '報表', icon:'document',route:'/form'},
-          {name: '圖片', icon:'Picture',route:'/picture'},
-          {name: '工具 / 樣式', icon:'Handbag',route:'/tool'}
-        ])
+        const nowMenu = ref([])
         const isCollapse = ref(false)
         const handleOpen = (key, keyPath) => {
           // console.log('open',key, keyPath)
@@ -77,36 +72,32 @@ div(class="w-auto h-[100vh] border-r-2")
         }
         const toLink = (item) => {
           if(!item.route) return false
-          // console.log('gogo', item.route)
           router.push(item.route)
           // const query = JSON.parse(JSON.stringify(route))
-          if(item.route === '/game') {
-            menuData.value = [
-              {name: '首頁', icon:'House',route:'/'},
-              {name: '猜數字', icon:'MagicStick'},
-              {name: '記憶遊戲', icon:'MagicStick'},
-              {name: '踩地雷', icon:'MagicStick'},
-            ]
+          setMenu(item.route)
+        }
+
+        const setMenu = (routePath = '') => {
+          if(routePath === '/game') {
+            nowMenu.value = menuData.value.game
           } else {
-            menuData.value = [
-              {name: '首頁', icon:'House',route:'/'},
-              {name: '遊戲', icon:'MagicStick',route:'/game'},
-              {name: '報表', icon:'document',route:'/form'},
-              {name: '圖片', icon:'Picture',route:'/picture'},
-              {name: '工具 / 樣式', icon:'Handbag',route:'/tool'}
-            ]
+            nowMenu.value = menuData.value.original
           }
         }
-        const init = () => {
 
+        const init = () => {
+          setMenu()
         }
+        init()
+
         return {
           isCollapse,
           handleOpen,
           handleClose,
           Switch,
-          menuData,
+          nowMenu,
           toLink,
+          defaultActive,
         }
       }
     }
