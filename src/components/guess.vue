2674309f4097f8e3605d784d8f33c10653f7ae2a
jxes6102 <jxes6102@gamil.com>
2022-09-20 17:09:20 +0800

    temp09201700

--- src/components/guess.vue
+++ src/components/guess.vue
@@ -1,6 +1,6 @@
 <template lang='pug'>
-div(class="w-[100vw] md:w-[50vw] h-[80vh] md:h-[90vh] flex flex-col items-center justify-start bg-[#ffffff]")
-  div(class="flex p-2 justify-center items-center")
+div(class="w-[100vw] md:w-[50vw] h-[80vh] md:h-[90vh] flex flex-col items-center justify-start")
+  div(class="flex p-1 justify-center items-center")
     input(
       v-model="guessNum"
       :class="[ \
@@ -16,60 +16,53 @@ div(class="w-[100vw] md:w-[50vw] h-[80vh] md:h-[90vh] flex flex-col items-center
     ) 送出
     button(
       class='bg-red-600 text-white ml-3 py-2 px-4 font-medium rounded-xl transition-all duration-300 hover:bg-red-500'
-      @click="madeNum"
+      @click="tipStatus = !tipStatus"
     ) 提示
   div(v-if="errorMessage" class="text-base text-rose-600") {{errorMessage}}
-  div(class="w-full flex p-2 justify-around items-center")
+  div(class="w-full flex mt-1 p-2 justify-around items-center")
     div 數字
     div AB
-    div 提示
+    div(v-if="tipStatus") 提示
   div(
     ref="mainView"
-    class="w-full h-auto max-h-[50vh] md:max-h-[55vh] overflow-auto"
+    class="w-full h-auto max-h-[50vh] md:max-h-[55vh] mt-2 overflow-auto"
   )
     div(v-for="item in data" class="w-full flex p-2 justify-around items-center")
       div {{item.guessNum}}
       div {{item.AB}}
-      div {{item.tip}}
+      div(v-if="tipStatus") {{item.tip}}
   div(v-if="endStatus" class="w-full h-auto overflow-auto flex flex-col")
-    div(class="text-xl text-red-700") 猜中了 共猜了?次
+    div(class="my-1 text-xl text-red-700") 猜中了 共猜了{{times}}次
     div()
       button(
-        class='bg-green-500 text-white ml-3 py-2 px-4 font-medium rounded-xl transition-all duration-300 hover:bg-green-400'
+        class='bg-green-500 text-white py-2 px-4 font-medium rounded-xl transition-all duration-300 hover:bg-green-400'
         @click="madeNum"
       ) 再玩一次
-
 </template>
 <script>
   // @ is an alias to /src
   import { ref, nextTick } from 'vue'
-  import { checkGuessNum } from '@/utils/valid'
   export default {
     name: 'guessView',
     components: {
     },
-    setup(prop, {emit}) {
+    setup() {
       const data = ref([])
       const guessNum = ref(null)
       const errorMessage = ref('')
       const endStatus = ref(false)
+      const tipStatus = ref(false)
       let trueNum = ''
       const mainView = ref(null)
-
-      const action = () => {
-        errorMessage.value = checkGuessNum(guessNum.value)
-        if(errorMessage.value || endStatus.value) return false
-        madeAB()
-        nextTick(() => {
-          mainView.value.scrollTop = 9999
-        })
-      }
+      const times = ref(0)
 
       const madeNum = () => {
         endStatus.value = false
         data.value = []
         let target = ''
         guessNum.value = null
+        times.value = 0
+
         const base = ['0','1','2','3','4','5','6','7','8','9']
         while(target.length < 4) {
           const random = base[Math.floor(Math.random() * 10)]
@@ -78,32 +71,61 @@ div(class="w-[100vw] md:w-[50vw] h-[80vh] md:h-[90vh] flex flex-col items-center
         trueNum = target
         console.log('trueNum', trueNum)
       }
+
       madeNum()
+
+      const checkGuessNum = (val) => {
+        let errMessage = ''
+        if(!val) errMessage = '請輸入數字'
+        else if (val.length !== 4) errMessage = '長度須為4'
+        else if (isNaN(val)) errMessage = '只能輸入數字'
+        // else if (val.split('').some(x => val.split('').indexOf(x) !== val.split('').lastIndexOf(x))) errMessage = '數字不能重覆'
+        else if (new Set(val.split('')).size !== val.split('').length) errMessage = '數字不能重覆'
+        return errMessage
+      }
+
+      const action = () => {
+        errorMessage.value = checkGuessNum(guessNum.value)
+        if(errorMessage.value || endStatus.value) return false
+
+        times.value++
+        madeAB()
+
+        nextTick(() => {
+          mainView.value.scrollTop = mainView.value.scrollHeight
+        })
+      }
+
       const madeAB = () => {
         let a = 0
         let b = 0
+        let aTip = []
+        let bTip = []
         for(let i = 0; i < guessNum.value.length ; i++) {
           if(guessNum.value.charAt(i) === trueNum.charAt(i)) {
             a++
+            aTip.push(trueNum.charAt(i))
           } else if (trueNum.includes(guessNum.value.charAt(i))) {
             b++
+            bTip.push(trueNum.charAt(trueNum.indexOf(guessNum.value.charAt(i))))
           }
         }
-        data.value.push(
-          {guessNum:guessNum.value, AB:a+'A'+b+'B', tip:'none'}
-        )
+
+        const alltip =  (a ? "A:" + aTip.toString() : '') + (b ? " B:" + bTip.toString() : '')
+        data.value.push({guessNum:guessNum.value, AB:a+'A'+b+'B', tip:alltip })
         if(a === 4) endStatus.value = true
-        // console.log(a+'A'+b+'B')
       }
 
       return {
         guessNum,
         data,
-        action,
         errorMessage,
-        madeNum,
         endStatus,
         mainView,
+        tipStatus,
+        times,
+        madeNum,
+        action,
       }
     }
   }
--- src/views/gameView.vue
+++ src/views/gameView.vue
@@ -1,6 +1,6 @@
 <template lang='pug'>
 div(class="w-full h-[100vh] flex flex-col items-center justify-start")
-  div(class="text-3xl") {{displayName}}
+  div(class="text-3xl my-2") {{displayName}}
   component(:is="gameName")
 </template>
 <script>
