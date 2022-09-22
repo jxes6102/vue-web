5f5e58f79318a7cabe93376e8640f053f93cab3a
jxes6102 <jxes6102@gamil.com>
2022-09-22 16:32:59 +0800

    temp09221635

--- src/components/landmine.vue
+++ src/components/landmine.vue
@@ -1,16 +1,29 @@
 <template lang='pug'>
-div(class="")
-  h1 landmine view
+div(class="w-[300px] md:w-[450px] h-auto flex flex-col items-center justify-start bg-slate-50")
+  div(class="flex flex-wrap justify-center items-center")
+    div(
+      v-for="item in data"
+      class="w-[20px] h-[20px] md:w-[35px] md:h-[35px] m-1 flex justify-center items-center bg-lime-400 "
+    ) {{item}}
 </template>
 <script>
   // @ is an alias to /src
+  import { ref } from 'vue'
   export default {
     name: 'landmineView',
     components: {
     },
     setup() {
+      const data = ref([])
+      // 10 12
+      for(let i = 0;i<120;i++) {
+        data.value.push(i + 1)
+      }
+      console.log(data.value.length)
+
 
       return {
+        data
       }
     }
   }
