5cb81b6a624cd0f91b41a90af32f9116faa3cf7a
jxes6102 <jxes6102@gamil.com>
2022-09-23 15:52:36 +0800

    temp09231600

--- src/components/landmine.vue
+++ src/components/landmine.vue
@@ -1,10 +1,11 @@
 <template lang='pug'>
-div(class="w-[310px] md:w-[400px] h-auto flex flex-col items-center justify-start bg-slate-50")
+div(class="w-[310px] md:w-[400px] h-auto flex flex-col items-center justify-start")
   div(class="flex flex-wrap justify-center items-center")
     div(v-for="(item, index) in land[0].length")
       div(
         v-for="(items, indexs) in land"
-        class="w-[30px] h-[30px] md:w-[40px] md:h-[40px] m-[2px] flex justify-center items-center bg-lime-400 "
+        :class="[land[indexs][index].check ? 'bg-[#fcfccf]' : 'bg-[#79fcac]']"
+        class="w-[30px] h-[30px] md:w-[40px] md:h-[40px] border-2 border-[#b59ec2] flex justify-center items-center "
         @click="action(indexs,index)"
       ) {{land[indexs][index].display}}
 </template>
@@ -17,22 +18,40 @@ div(class="w-[310px] md:w-[400px] h-auto flex flex-col items-center justify-star
     },
     setup() {
       const land = ref([])
-      for(let i = 0;i<10;i++) {
-        land.value[i] = []
-        for(let j = 0;j<8;j++) {
-          land.value[i][j] = {
-            isBoom:false,
-            flag:false,
-            display:0,
+      const init = () => {
+        for(let i = 0;i<10;i++) {
+          land.value[i] = []
+          for(let j = 0;j<8;j++) {
+            land.value[i][j] = {
+              isBoom:false,
+              flag:false,
+              display:0,
+              check:false,
+            }
           }
         }
+        madeBoom()
       }
-      console.log(land.value)
+
+      const madeBoom = () => {
+        let count = 15
+        for(let i = 0;i<count;) {
+          const x = Math.floor(Math.random() * 10)
+          const y = Math.floor(Math.random() * 8)
+          if(!land.value[x][y].isBoom){
+            land.value[x][y].isBoom = true
+            i++
+          }
+        }
+        console.log(land.value)
+      }
+      init()
 
       const action = (x, y) => {
+        land.value[x][y].display = 'q'
+        land.value[x][y].check = true
         console.log(land.value)
         console.log(x, y, land.value[x][y])
-        land.value[x][y].display = 'q'
       }
 
       return {
