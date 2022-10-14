07b8095ea143ee5fb99dfcbbb56b1f056a21dc17
jxes6102 <jxes6102@gamil.com>
2022-10-14 15:16:54 +0800

    temp20221520

--- src/views/formView.vue
+++ src/views/formView.vue
@@ -26,9 +26,9 @@ div(class="w-full h-[100vh] flex flex-wrap items-center justify-center")
       v-if="tableData.length"
       :data='tableData' :height='isMobile ? 300 : 500' style='width: 100%'
     )
-      el-table-column(prop='date' label='Date' width='180')
-      el-table-column(prop='name' label='Name' width='180')
-      el-table-column(prop='address' label='Address')
+      el-table-column(prop='A1' label='Date' width='200')
+      el-table-column(prop='B1' label='Name' width='200')
+      el-table-column(prop='C1' label='Address' width='200')
 </template>
 <script>
   // @ is an alias to /src
@@ -41,6 +41,7 @@ div(class="w-full h-[100vh] flex flex-wrap items-center justify-center")
     },
     setup() {
       const isMobile = computed(() => store.state.isMobile)
+      const tableTitle = ref([])
       const tableData = ref([])
       const init = () => {
         tableData.value = [
@@ -65,7 +66,6 @@ div(class="w-full h-[100vh] flex flex-wrap items-center justify-center")
           address: 'No. 189, Grove St, Los Angeles',
         },
       ]
-
       }
 
       const fileInput = ref(null)
@@ -75,16 +75,12 @@ div(class="w-full h-[100vh] flex flex-wrap items-center justify-center")
         fileInput.value.click()
       }
       const changeFile = () => {
-        if(fileInput.value.files.length === 1) {
-          console.log('changeFile', fileInput.value.files)
-          dealFile(fileInput.value.files[0])
-        }
+        if(fileInput.value.files.length === 1) dealFile(fileInput.value.files[0])
       }
       const dropFile = (e) => {
         e.preventDefault()
         borderStyle.value = false
         if (e.dataTransfer.files.length === 1) {
-          console.log('dropFile', e.dataTransfer.files)
           fileInput.value.files = e.dataTransfer.files
           dealFile(e.dataTransfer.files[0])
         }
@@ -99,40 +95,60 @@ div(class="w-full h-[100vh] flex flex-wrap items-center justify-center")
       }
 
     const dealFile = (file) => {
-      // is img
-      // if (file.type.startsWith("image/")) {
-      //   const reader = new FileReader()
-      //   reader.readAsDataURL(file)
-      //   reader.onload = () => {
-      //     fileDiv.value.style.backgroundImage = `url('${reader.result}')`
-      //   }
-      // } else {
-      //   fileDiv.value.style.backgroundImage = null
-      // }
+      console.log('type',file.type)
+      const reader = new FileReader()
+      if (file.type.startsWith("image/")) {
+        reader.readAsDataURL(file)
+        reader.onload = () => {
+          fileDiv.value.style.backgroundImage = `url('${reader.result}')`
+          // fileDiv.value.style.backgroundImage = null
+        }
+      } else if (file.type.includes('spreadsheetml.sheet')) {
+        reader.readAsArrayBuffer(file)
+        reader.onload = () => {
+            const data = new Uint8Array(reader.result)
+            const wb = XLSX.read(data, {type:'array'})
+            // console.log('wb', wb)
+            if(!wb.Sheets[wb.SheetNames[0]]['!merges'] && wb.Sheets[wb.SheetNames[0]]['!ref'].includes(':C')) {
+              console.log('成功',wb.Sheets[wb.SheetNames[0]])
+              let range = parseInt(wb.Sheets[wb.SheetNames[0]]['!ref'].split(':C')[1])
+              // console.log('range', range)
+              let data = []
 
-      // is excel
-      const reader = new FileReader();
-      reader.readAsArrayBuffer(file)
+              // console.log('values',Object.values(wb.Sheets[wb.SheetNames[0]]))
+              // console.log('keys',Object.keys(wb.Sheets[wb.SheetNames[0]]))
+              for(let item of Object.keys(wb.Sheets[wb.SheetNames[0]])) {
+                let index = parseInt(item.replace(/[^0-9']/g, ''))
+                if (!index) continue
+                if(!data[index]) data[index] = {}
+                data[index][item] = wb.Sheets[wb.SheetNames[0]][item]?.w
+              }
+              // console.log('data',data)
 
-      reader.addEventListener("loadend", function() {
-          const data = new Uint8Array(reader.result);
-          const wb = XLSX.read(data, {type:'array'});
-          console.log('wb', wb)
-          // process_wb(wb);
-      });
+              let tatget = []
+              for(let i = 1;i<=range;i++) {
+                tatget.push({
+                  A1:data[i]['A'+i],
+                  B1:data[i]['B'+i],
+                  C1:data[i]['C'+i],
+                })
+              }
+              tableData.value = tatget
+              // console.log('tatget',tatget)
+            } else {
+              console.log('有合併儲存格或超出範圍(only A~C)')
+            }
+
+        }
+      }
+
 }
 
     onMounted(() => {
+
       // init()
-      // loadFile()
+
     })
-
-
-    function process_wb(wb) {
-      XLSX.utils.sheet_to_html(wb.Sheets[wb.SheetNames[0]], {editable:true}).replace("<table", '<table id="table" border="1"');
-    }
-
-
 
       return {
         isMobile,
