a87d1e6b92e6a2bb6e6ed8600b0b658b165bfde8
jxes6102 <jxes6102@gamil.com>
2022-10-20 16:47:43 +0800

    temp101011650

--- src/components/cubeView.vue
+++ src/components/cubeView.vue
@@ -1,11 +1,63 @@
 <template lang='pug'>
 div(
-  class="fixed top-0 left-0 w-screen h-[100vh] bg-zinc-700 opacity-90 overflow-x-hidden overflow-y-auto flex items-center justify-center"
+  class="fixed top-0 left-0 w-screen h-[100vh] bg-zinc-700 opacity-90 flex items-center justify-center"
   @click.self="close"
 )
-  div(
-    class="w-10 h-10 bg-white"
-  ) cubeView
+  .body
+    .wrap(
+      class=""
+    )
+      .cube(
+        class=""
+      )
+        div(
+          class=""
+          style="--x:-1;--y:0;"
+        )
+          span(
+            class=""
+            style="--i:3"
+          )
+          span(
+            class=""
+            style="--i:2"
+          )
+          span(
+            class=""
+            style="--i:1"
+          )
+        div(
+          class=""
+          style="--x:0;--y:0;"
+        )
+          span(
+            class=""
+            style="--i:3"
+          )
+          span(
+            class=""
+            style="--i:2"
+          )
+          span(
+            class=""
+            style="--i:1"
+          )
+        div(
+          class=""
+          style="--x:1;--y:0;"
+        )
+          span(
+            class=""
+            style="--i:3"
+          )
+          span(
+            class=""
+            style="--i:2"
+          )
+          span(
+            class=""
+            style="--i:1"
+          )
 </template>
 <script>
   // @ is an alias to /src
@@ -24,5 +76,39 @@ div(
   }
 </script>
 <style scoped>
-
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-color: #25335b;
+}
+.wrap {
+  position: relative;
+  top: -80px;
+  transform: skewY(-20deg);
+}
+.wrap .cube {
+  z-index: 2;
+}
+.wrap .cube div {
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  translate: calc(-70px *var(--x)) calc(-60px*var(--y));
+}
+.wrap .cube div span {
+  position: relative;
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  background-color: #dcdcdc;
+  z-index: calc(1*var(--i));
+}
 </style>
