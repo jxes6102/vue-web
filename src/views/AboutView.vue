b3e42829a6b50789f2e40874a81e743aa0d935f5
jxes6102 <jxes6102@gamil.com>
2022-10-31 07:26:27 +0800

    temp10311930

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,24 @@
 <template lang='pug'>
-div(class="about")
+div(class="about flex flex-col")
+  nav.dropdownmenu
+    ul
+      li
+        a() Home
+      li
+        a() About Me
+      li
+        a(href='#') Articles on HTML5 & CSS3
+        ul#submenu
+          li
+            a() Difference between SVG vs. Canvas
+          li
+            a() New features in HTML5
+          li
+            a() Creating links to sections within a webpage
+      li
+        a() News
+      li
+        a() Contact Us
   h1(class="text-lg text-teal-300") This is an about page
   h1 {{$t('__hello_world')}}
   h1(
@@ -32,3 +51,59 @@ div(class="about")
     }
   }
 </script>
+<style scoped>
+.dropdownmenu ul, .dropdownmenu li {
+	margin: 0;
+	padding: 0;
+}
+.dropdownmenu ul {
+	background: gray;
+	list-style: none;
+	width: 100%;
+}
+.dropdownmenu li {
+	float: left;
+	position: relative;
+	width:auto;
+}
+.dropdownmenu a {
+	background: #30A6E6;
+	color: #FFFFFF;
+	display: block;
+	font: bold 12px/20px sans-serif;
+	padding: 5px 10px;
+	text-align: center;
+	text-decoration: none;
+	-webkit-transition: all .25s ease;
+	-moz-transition: all .25s ease;
+	-ms-transition: all .25s ease;
+	-o-transition: all .25s ease;
+	transition: all .25s ease;
+}
+.dropdownmenu li:hover a {
+	background: #000000;
+}
+#submenu {
+	left: 0;
+	opacity: 0;
+	position: absolute;
+	top: 35px;
+	visibility: hidden;
+	z-index: 1;
+}
+li:hover ul#submenu {
+	opacity: 1;
+	top: 40px;	/* adjust this as per top nav padding top & bottom comes */
+	visibility: visible;
+}
+#submenu li {
+	float: none;
+	width: 100%;
+}
+#submenu a:hover {
+	background: #DF4B05;
+}
+#submenu a {
+	background-color:#000000;
+}
+</style>
