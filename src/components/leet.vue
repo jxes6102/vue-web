d081ca236f0b91aa9d4d00afadac67685b52576b
jxes6102 <jxes6102@gamil.com>
2023-02-17 11:36:04 +0800

    temp021171140

--- src/components/leet.vue
+++ src/components/leet.vue
@@ -11,7 +11,36 @@ export default {
     msg: String
   },
   setup() {
-    console.log('leet')
+    console.log('leet now')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // 1551. Minimum Operations to Make Array Equal
+    // let n = 6
+    // let arr = [1]
+    // for(let i = 1;i<n;i++){
+    //   arr.push(arr[i-1]+2)
+    // }
+    // let avg = (arr.length%2 === 1) ? arr[Math.floor(arr.length/2)] : arr[Math.floor(arr.length/2) - 1] + 1
+    // let ans = 0
+    // for(let i = 0;i<arr.length;i++){
+    //   ans += Math.abs(arr[i] - avg)
+    // }
+    // console.log('ans',ans/2)
 
   }
 }
