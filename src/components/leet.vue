3e4007f5dd0fd1e59ef0d541a017db8c5b882f25
jxes6102 <jxes6102@gamil.com>
2023-02-18 15:42:24 +0800

    temp02181545

--- src/components/leet.vue
+++ src/components/leet.vue
@@ -13,38 +13,48 @@ export default {
   setup() {
     console.log('leet now')
 
-    let garbage = ["G","P","GP","GG"], travel = [2,4,3]
-    let type = ["M","P","G"]
-    let str = garbage.join('')
-    let garbageM = garbage.map((item)=> item.indexOf("M") >=0 ? item : '')
-    let garbageP = garbage.map((item)=> item.indexOf("P") >=0 ? item : '')
-    let garbageG = garbage.map((item)=> item.indexOf("G") >=0 ? item : '')
-    let arr = [garbageM,garbageP,garbageG]
-    let ans = 0
-    // console.log(garbageM)
-    // console.log(garbageP)
-    // console.log(garbageG)
-    // console.log(arr)
-    // console.log(arr.findIndex((item)=>item.join('') === ''))
-    while(arr.findIndex((item)=>item.join('') === '') >=0) {
-      type.splice(arr.findIndex((item)=>item.join('') === ''),1)
-      arr.splice(arr.findIndex((item)=>item.join('') === ''),1)
-    }
-
-    for(let i = 0;i<arr.length;i++){
-
-      console.log(arr[i])
-
-    }
-    console.log(type)
-    console.log(arr)
 
 
 
 
+    // 2373. Largest Local Values in a Matrix
+    // let grid = [[9,9,8,1],[5,6,2,6],[8,2,6,4],[6,2,2,2]]
+    // let ans = []
+
+    // for(let i = 0;i<grid.length;i++) {
+    //   for(let j = 0;j<grid[i].length;j++){
+    //     if(i-1>=0 && i+1<grid[i].length && j-1>=0 && j+1<grid[i].length){
+    //       if(!ans[i-1]) ans[i-1] = []
+    //       ans[i-1][j-1] = Math.max(...[grid[i][j],grid[i+1][j],grid[i-1][j],grid[i][j+1],grid[i][j-1],grid[i+1][j+1],grid[i-1][j-1],grid[i+1][j-1],grid[i-1][j+1]])
+    //     }
+    //   }
+    // }
+
+    // console.log('ans',ans)
 
 
+    // 2391. Minimum Amount of Time to Collect Garbage
+    // let garbage = ["MMM","PGM","GP"], travel = [3,10]
+    // let type = ["M","P","G"]
+    // let str = garbage.join('')
+    // let garbageM = garbage.map((item)=> item.indexOf("M") >=0 ? item : '')
+    // let garbageP = garbage.map((item)=> item.indexOf("P") >=0 ? item : '')
+    // let garbageG = garbage.map((item)=> item.indexOf("G") >=0 ? item : '')
+    // let arr = [garbageM,garbageP,garbageG]
+    // let ans = 0
 
+    // while(arr.findIndex((item)=>item.join('') === '') >=0) {
+    //   type.splice(arr.findIndex((item)=>item.join('') === ''),1)
+    //   arr.splice(arr.findIndex((item)=>item.join('') === ''),1)
+    // }
+
+    // for(let i = 0;i<arr.length;i++){
+    //   while(arr[i][arr[i].length - 1] === '') arr[i].pop()
+    //   ans += travel.slice(0,arr[i].length - 1).reduce((acc, cur) => acc + cur,0)
+    // }
+    // ans+=str.length
+
+    // console.log('ans',ans)
 
 
     // 1551. Minimum Operations to Make Array Equal
